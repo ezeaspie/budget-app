96460cd1f8c4ead7757bb9cd80e449aa340d1cfb
Ezequiel Espinoza <ezequielnoza@gmail.com>
2018-01-18 18:52:42 -0500

    Added error checking and localStorage functionality.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # budget-app
 A simple budgeting app that allows the user to add income and expenses and track surplus finances.
+
+-Added localStorage support
+-
--- scripts.js
+++ scripts.js
@@ -1,12 +1,61 @@
-let incomeArr = [];
-let expenseArr = [];
+var incomeArr = JSON.parse(localStorage.getItem("incomeArr") || '[]');
+var expenseArr = JSON.parse(localStorage.getItem("expenseArr") || '[]');
 
+//Intitialize
+const paintCards = (array, dataName, whereToRender) => {
+  let HTML = '';
+  for (i = 0 ; i<array.length; i++){
+      let currentItem = array[i];
+      HTML += `
+        <li class='card'>
+          <h2>${currentItem.name}</h2>
+          <h3>Amount: ${currentItem.amount}</h3>
+          <button class="remove" data-index = "${i}" data-array="${dataName}">Remove Book</button>
+        </li>`
+      }
+  $(whereToRender).html(HTML);
+}
+
+const updateDisplay = () => {
+  let totalIncome = 0;
+  let totalExpense = 0;
+
+  for (i = 0 ; i<incomeArr.length; i++){
+    let currentItem = incomeArr[i];
+    totalIncome += Number(currentItem.amount);
+  };
+  for (i = 0 ; i<expenseArr.length; i++){
+    let currentItem = expenseArr[i];
+    totalExpense += Number(currentItem.amount);
+  };
+
+  let surplus = totalIncome - totalExpense;
+
+  $('#totalIncome').html(`${totalIncome}.00`);
+  $('#totalExpense').html(`${totalExpense}.00`);
+  $('#totalSurplus').html(`${surplus}.00`);
+  $('#bar').attr("value", surplus);
+  $('#bar').attr("max", totalIncome);
+}
+
+$('#incomeError').hide();
+$('#expenseError').hide();
+
+paintCards(incomeArr, 'income', '#incomeList');
+
+paintCards(expenseArr, 'expense', '#expenseList');
+
+updateDisplay();
 //Functions
 
 const getInputVal = (selector) => {
   let inputVal = $(selector).val();
   return inputVal;
 };
+
+const clearInputVal = (selector) => {
+  $(selector).val('');
+}
 
 const NewIncomeExpense = (name,amount) => {
    return { name, amount };
@@ -27,50 +76,44 @@ const renderCards = (arr, objectToPush, divToPrintTo, arrId) => {
   $(divToPrintTo).html(HTML);
 };
 
-const updateDisplay = () => {
-  let totalIncome = 0;
-  let totalExpense = 0;
-
-  for (i = 0 ; i<incomeArr.length; i++){
-    let currentItem = incomeArr[i];
-    totalIncome += Number(currentItem.amount);
-  };
-  for (i = 0 ; i<expenseArr.length; i++){
-    let currentItem = expenseArr[i];
-    totalExpense += Number(currentItem.amount);
-  };
-
-  let surplus = totalIncome - totalExpense;
-
-  $('#totalIncome').html(`${totalIncome}.00`);
-  $('#totalExpense').html(`${totalExpense}.00`);
-  $('#totalSurplus').html(`${surplus}.00`);
-}
-
 //Print card/Store Data when forms are submitted.
 
 $(document).on("submit", "#add", function(e){
   e.preventDefault();
   //Get values of each input and store in variable.
   let incomeName = getInputVal('#incomeName');
-  let incomeAmount = getInputVal('#incomeAmount');
-  console.log(incomeAmount, incomeName);
-  //Push that input into an object, then into an array. Store the index.
-  let currentObj = NewIncomeExpense(incomeName,incomeAmount);
-  renderCards(incomeArr,currentObj,'#incomeList', "income");
-  updateDisplay();
+  let incomeAmount = Number(getInputVal('#incomeAmount'));
+  if (incomeAmount < 0 || incomeName === ''){
+    $('#incomeError').show();
+  }
+  else {
+    //Push that input into an object, then into an array. Store the index.
+    $('#incomeError').hide();
+    let currentObj = NewIncomeExpense(incomeName,incomeAmount);
+    renderCards(incomeArr,currentObj,'#incomeList', "income");
+    localStorage.setItem("incomeArr", JSON.stringify(incomeArr));
+    clearInputVal('#incomeName, #incomeAmount');
+    updateDisplay();
+  }
 });
 
 $(document).on("submit", "#subtract", function(e){
   e.preventDefault();
   //Get values of each input and store in variable.
   let expenseName = getInputVal('#expenseName');
-  let expenseAmount = getInputVal('#expenseAmount');
-  console.log(expenseAmount, expenseName);
-  //Push that input into an object, then into an array. Store the index.
-  let currentObj = NewIncomeExpense(expenseName,expenseAmount);
-  renderCards(expenseArr,currentObj,'#expenseList', "expense");
-  updateDisplay();
+  let expenseAmount = Number(getInputVal('#expenseAmount'));
+  if (expenseAmount < 0 || expenseName === ''){
+    $('#expenseError').show();
+  }
+  else {
+    //Push that input into an object, then into an array. Store the index.
+    $('#expenseError').hide();
+    let currentObj = NewIncomeExpense(expenseName,expenseAmount);
+    renderCards(expenseArr,currentObj,'#expenseList', "expense");
+    localStorage.setItem("expenseArr", JSON.stringify(expenseArr));
+    clearInputVal('#expenseName, #expenseAmount');
+    updateDisplay();
+  }
 });
 
 $(document).on("click",".remove",function(){
@@ -79,33 +122,13 @@ $(document).on("click",".remove",function(){
   console.log(id);
   if (id === "income") {
     incomeArr.splice(index,1);
-    //Modularize later
-    let HTML = '';
-    for (i = 0 ; i<incomeArr.length; i++){
-        let currentItem = incomeArr[i];
-        HTML += `
-          <li class='card'>
-            <h2>${currentItem.name}</h2>
-            <h3>Amount: ${currentItem.amount}</h3>
-            <button class="remove" data-index = "${i}" data-array="income">Remove Book</button>
-          </li>`
-        }
-    $('#incomeList').html(HTML);
+    paintCards(incomeArr, 'income', '#incomeList');
   }
   else {
     expenseArr.splice(index,1);
-    //Modularize
-    let HTML = '';
-    for (i = 0 ; i<expenseArr.length; i++){
-        let currentItem = expenseArr[i];
-        HTML += `
-          <li class='card'>
-            <h2>${currentItem.name}</h2>
-            <h3>Amount: ${currentItem.amount}</h3>
-            <button class="remove" data-index = "${i}" data-array="expense">Remove Book</button>
-          </li>`
-        }
-    $('#expenseList').html(HTML);
+    paintCards(expenseArr, 'expense', '#expenseList');
   }
+  localStorage.setItem("incomeArr", JSON.stringify(incomeArr));
+  localStorage.setItem("expenseArr", JSON.stringify(expenseArr));
   updateDisplay();
 });
